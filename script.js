5967de9b4f798fc6d337210f026b85ae79f31d96
Dishant Upadhyay <dishantu2004@gmail.com>
2025-06-12 18:03:44 +0530

    Updated project with latest changes

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const navLinks = document.getElementById('navLinks');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 });
+
+AOS.init({
+    duration: 1000,
+    once: false
+});

37b53cf7a9b1016800850c7b6f4ad946a6e40583
Dishant Upadhyay <dishantu2004@gmail.com>
2025-06-16 10:11:16 +0530

    Updated styles and added separate CSS files for each page

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ AOS.init({
     duration: 1000,
     once: false
 });
+
+const emojiFavicon = document.createElement('link');
+emojiFavicon.rel = 'icon';
+emojiFavicon.href = 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌍</text></svg>';
+document.head.appendChild(emojiFavicon);
